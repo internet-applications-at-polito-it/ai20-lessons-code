b2db8262023c51793bd3344bd072e38bd5b60855
Antonio Servetti <antonio.servetti@polito.it>
2020-03-31 16:46:13 +0200

    New project L0510-framework-first-component

---
+++ projects/L0510-framework-first-component/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ai19';
+  toggleTitle(event: Event) {
+    this.title = (this.title === 'ai19') ? 'Applicazioni Internet (2019)' : 'ai19';
+  }
+}
