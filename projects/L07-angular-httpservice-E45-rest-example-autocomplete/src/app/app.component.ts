1589ad8d9454f34ed2364ad4883d424d13efb9b4
Antonio Servetti <servetti-polito@users.noreply.github.com>
2020-04-07 21:48:53 +0200

    Completed L07-angular-httpservice-E45-rest-example-autocomplete

--- projects/L07-angular-httpservice-E45-rest-example-autocomplete/src/app/app.component.ts
+++ projects/L07-angular-httpservice-E45-rest-example-autocomplete/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import {Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, ViewChild, AfterViewInit, ElementRef, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
-import {GoogleBooksService} from './google-books.service';
+import { GoogleBooksService } from './google-books.service';
 import { Book } from './models/book.model';
-import { Observable } from 'rxjs';
+
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  map,
+  concatMap,
+  switchMap,
+  withLatestFrom,
+  concatAll, shareReplay
+} from 'rxjs/operators';
+import {merge, fromEvent, Observable, concat} from 'rxjs';
+
 
 /**
  * @title Data table with sorting, pagination, and filtering.
@@ -24,6 +38,7 @@ export class AppComponent implements AfterViewInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('searchInput', { static: true }) input: ElementRef;
 
   constructor(private googleBooksService: GoogleBooksService) {
     // Assign the data to the data source for the table to render
@@ -42,13 +57,32 @@ export class AppComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
     */
+
+    // console.dir(this.input.nativeElement);
+
+    const searchBooks$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+      .pipe(
+        tap( (event) => {
+          console.log(`The input value is ${event.target.value}`);
+          return event;
+        }),
+        map(event => event.target.value),
+        debounceTime(400),
+        distinctUntilChanged(),
+        switchMap(search => this.queryFilter(search))
+      );
+    const initialBooks$ = this.queryFilter('');
+    this.dataSource$ = concat(initialBooks$, searchBooks$);
+
   }
 
-  queryFilter(filterValue: string) {
+  queryFilter(filterValue: string = ''): Observable<Book[]> {
+    console.log(`queryFilter(${filterValue})`);
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     // this.dataSource.filter = filterValue;
-    this.dataSource$ = this.googleBooksService.searchBooks(filterValue);
+    // this.dataSource$ = this.googleBooksService.searchBooks(filterValue);
+    return this.googleBooksService.searchBooks(filterValue);
   }
 
   retrieveBook(volumeId: string) {
