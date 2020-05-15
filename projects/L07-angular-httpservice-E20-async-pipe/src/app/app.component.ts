1a44b54815feb10b05a55571a718c33fff6fe430
Antonio Servetti <servetti-polito@users.noreply.github.com>
2020-05-15 19:29:47 +0200

    L07-angular-httpservice-E20-async-pipe project completed

--- projects/L07-angular-httpservice-E20-async-pipe/src/app/app.component.ts
+++ projects/L07-angular-httpservice-E20-async-pipe/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'L07-angular-httpservice-E20-async-pipe';
+
+  constructor() {}
 }
--- projects/L07-angular-httpservice-E20-async-pipe/src/app/app.module.ts
+++ projects/L07-angular-httpservice-E20-async-pipe/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
+import { HeroListComponent } from './hero-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, HeroListComponent ],
+  providers:    [ HeroService ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
